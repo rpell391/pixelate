708389a1c2368e2b5591dca27f8f70163b9d202e
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-10-03 11:51:18 -0400

    added eventlistener and colors styles

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // Your code here
-const table = document.getElementById("table1")[0];
+const table = document.getElementById("table1");
 function makeRow() {
   const row = document.createElement("tr");
   for (let i = 0; i < 20; i++) {
     const td = document.createElement("td");
     row.appendChild(td);
   }
-  table1.appendChild(row);
+  table.appendChild(row);
 }
 makeRow();
 makeRow();
+makeRow();
+
+const button = document.getElementById("add-row");
+button.addEventListener("click", makeRow);
