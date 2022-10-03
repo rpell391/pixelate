c2fc41cbf10063314d8f820a910c498dd95c8866
Randy Pellegrin <rpell391@gmail.com>
2022-10-03 09:48:39 -0500

    added two rows to our table

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Your code here
+const table = document.getElementById("table1")[0];
+function makeRow() {
+  const row = document.createElement("tr");
+  for (let i = 0; i < 20; i++) {
+    const td = document.createElement("td");
+    row.appendChild(td);
+  }
+  table1.appendChild(row);
+}
+makeRow();
+makeRow();
