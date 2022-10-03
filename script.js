8858a8a328127f24270cf2096dc9a03bc5cfb9c9
Alex <alexwinston2123@icloud.com>
2022-10-03 13:06:41 -0500

    added switching colors

--- script.js
+++ script.js
@@ -14,3 +14,34 @@ makeRow();
 
 const button = document.getElementById("add-row");
 button.addEventListener("click", makeRow);
+
+table.addEventListener('click', colorize)
+function colorize (event) {
+  const target = event.target
+    if (target.className.length) {
+        target.className = ''
+  } else {
+    target.className = 'red'
+  }
+}
+const select = document.getElementsByTagName('select')[0]
+function changeEvent(event){
+  console.log(event.target.value)
+}
+select.addEventListener('change', changeEvent)
+
+let chosenColor = 'red'
+
+select.addEventListener('change', function (event){
+  chosenColor = event.target.value
+})
+
+function colorize (event) {
+  const target = event.target
+    if (target.className.length) {
+        target.className = ''
+  } else {
+    target.className = chosenColor
+  }
+}
+
